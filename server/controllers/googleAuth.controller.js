cf6ab48509d796d98e5ce158465f06e40c4e1947
Amardeep <akio190711@gmail.com>
2025-11-24 17:57:45 +0530

    update

--- server/controllers/googleAuth.controller.js
+++ server/controllers/googleAuth.controller.js
@@ -65,8 +65,8 @@
 
       res.cookie("jwt", jwtToken, {
         httpOnly: true,
-        secure: false,
-        sameSite: "lax",
+        secure: true,
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000,
       });
 
